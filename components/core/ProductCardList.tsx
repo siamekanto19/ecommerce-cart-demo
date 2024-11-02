d18bc47b95f0861696d172e089a65a4f0b93d31a
Siam <siamekanto360@gmail.com>
2024-11-03 05:28:37 +0600

    fix css positioning

--- components/core/ProductCardList.tsx
+++ components/core/ProductCardList.tsx
@@ -36,24 +36,22 @@ const ProductCardList = () => {
           </div>
         ) : (
           <Fragment>
-            <div className='w-full flex flex-col md:flex-row justify-between'>
+            <div className='w-full flex flex-col md:flex-row justify-between gap-4'>
               <div className='relative flex items-center w-full md:w-1/2 group'>
                 <Input onChange={(e) => setKeyword(e.target.value)} placeholder='Search Products...' className='w-full pl-9 pr-4' />
                 <Search absoluteStrokeWidth strokeWidth={1.25} size={20} className='absolute left-2 text-muted-foreground group-focus:text-black' />
               </div>
-              <div>
-                <Select value={limit.toString()} onValueChange={(limit) => setLimit(parseInt(limit))}>
-                  <SelectTrigger className='w-[200px]'>
-                    <SelectValue placeholder='Product limit' />
-                    <SelectContent>
-                      <SelectItem value='5'>5 Products</SelectItem>
-                      <SelectItem value='10'>10 Products</SelectItem>
-                      <SelectItem value='25'>25 Products</SelectItem>
-                      <SelectItem value='50'>50 Products</SelectItem>
-                    </SelectContent>
-                  </SelectTrigger>
-                </Select>
-              </div>
+              <Select value={limit.toString()} onValueChange={(limit) => setLimit(parseInt(limit))}>
+                <SelectTrigger className='w-full md:w-[200px]'>
+                  <SelectValue placeholder='Product limit' />
+                  <SelectContent>
+                    <SelectItem value='5'>5 Products</SelectItem>
+                    <SelectItem value='10'>10 Products</SelectItem>
+                    <SelectItem value='25'>25 Products</SelectItem>
+                    <SelectItem value='50'>50 Products</SelectItem>
+                  </SelectContent>
+                </SelectTrigger>
+              </Select>
             </div>
             {data?.products.length ? (
               <div className='mt-5 w-full grid grid-flow-row grid-cols-1 @md:grid-cols-2 @3xl:grid-cols-3 @5xl:grid-cols-4 @7xl:grid-cols-5 gap-5'>
